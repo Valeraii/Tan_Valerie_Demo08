da1778d9ec47f413966b9f1b29351447c3cb023c
Valerie Tan <valerietan000@gmail.com>
2021-10-26 12:19:25 -0700

    finished demo

--- scripts/firebaseAPI.js
+++ scripts/firebaseAPI.js
@@ -1,12 +1,12 @@
 // Your web app's Firebase configuration
 var  firebaseConfig = {
-    apiKey: ,
-    authDomain: ",
-    projectId: ,
-    storageBucket: ,
-    messagingSenderId: ,
-    appId: 
-  };
+    apiKey: "AIzaSyC6rerKyHm9edSRGVUFRt2FkpkPiS8nOF4",
+    authDomain: "tanvaleriedemo08.firebaseapp.com",
+    projectId: "tanvaleriedemo08",
+    storageBucket: "tanvaleriedemo08.appspot.com",
+    messagingSenderId: "691137447317",
+    appId: "1:691137447317:web:65cee3a6831dbfaf61a29c"
+    };
   
 
   const app = firebase.initializeApp(firebaseConfig);
